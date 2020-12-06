9225131972a3c653c952e19311694c520105ed7b
vivienwika <74418175+vivienwika@users.noreply.github.com>
2020-12-06 20:24:23 +0100

    add responsive mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hi! Welcome to My Portfolio Site")
+console.log('Hi there! Welcome to our site.')
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
